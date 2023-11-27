cf2ac09faa851f1691682b0ac701bf083c658550
EyadAlazmeh2 <eyadoalazmeh@gmail.com>
2023-11-27 20:02:09 +0300

    Change the layout of the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box } from "@radix-ui/themes";
+import { Box, Flex } from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -16,26 +16,28 @@ const NavBar = () => {
     { label: "Issues", href: "/issues/list" },
   ];
   return (
-    <nav className="flex space-x-6 mb-5 h-14 px-5 border-b items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classNames({
-                "text-zinc-900": current === link.href,
-                "text-zinc-500": current !== link.href,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="flex space-x-6 mb-5 h-14 px-5 border-b items-center justify-between">
+      <Flex align="center" gap="5">
+        <Link href="/">
+          <AiFillBug />
+        </Link>
+        <ul className="flex space-x-6">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={classNames({
+                  "text-zinc-900": current === link.href,
+                  "text-zinc-500": current !== link.href,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Flex>
       <Box>
         {status === "authenticated" && (
           <Link href={"/api/auth/signout"}>Log out</Link>
